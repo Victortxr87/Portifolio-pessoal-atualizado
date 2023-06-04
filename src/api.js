6832ec26147b6858b363d1c25da6eac70a5c2fc6
Victortxr87 <82127678+Victortxr87@users.noreply.github.com>
2023-06-04 19:36:20 +0100

    link do render

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://bancodedados-api.onrender.com/api',
 });
 
 export default api;
